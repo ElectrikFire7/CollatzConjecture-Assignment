4f6edf6306bb94bd20b81bbe0b5b1f89037cb904
Kishlay Singh <72155843+Yolohama@users.noreply.github.com>
2022-11-18 08:32:36 +0530

    Add files via upload

---
+++ CollatzConjecture.c
@@ -0,0 +1,54 @@
+// Kishlay Singh
+// 201ME229
+// CS252 Assignment
+// Question No. 3.21
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+	int n=0;
+	int k=0;
+	
+	pid_t pid;
+
+		do
+		{
+			printf("Enter a positive integer to run the Collatz Conjecture.\n"); 
+  			scanf("%d", &k);	
+		}while (k <= 0);
+
+		pid = fork();
+
+		if (pid == 0)
+		{
+			printf("Pid = %d enters if block\n" , pid);
+			printf("Child is working...\n");
+			printf("%d\n",k);
+			while (k!=1)
+			{
+				if (k%2 == 0)
+				{
+					k = k/2;
+				}
+				else if (k%2 == 1)
+				{
+					k = 3 * (k) + 1;
+				}	
+			
+				printf("%d\n",k);
+			}
+		
+			printf("Child process (%d) is done.\n", pid);
+		}
+		else
+		{
+			printf("Pid = %d enters else block\n", pid);
+			printf("Parent process is waiting on child process...\n");
+			wait();
+			printf("Parent process (%d) is done.\n", pid);
+		}
+	return 0; 
+}
